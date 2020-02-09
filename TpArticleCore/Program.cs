e6bd185c4bf7aeaf8b0b051a26755ce3b9dff68c
Paul Colombier <paul.colombier@gmail.com>
2020-02-09 18:50:10 +0100

    Question 2 et 4

--- TpArticleCore/Program.cs
+++ TpArticleCore/Program.cs
@@ -10,7 +10,7 @@ namespace TPArticle
             Console.WriteLine("Hello World!");
 
             using (var db = new Model())
-            {
+            {   
                 // Creation du jeu de données
                 db.Add(new Secteur { 
                     Id = 1, 
@@ -105,7 +105,7 @@ namespace TPArticle
                 Console.WriteLine("");
                 
 
-                // Récupérer un article par son Id
+                // 1. Récupérer un article par son Id
                 Console.WriteLine("Selection d'un article");
                 var articleById = db.Article
                     .Find(1);
@@ -114,31 +114,69 @@ namespace TPArticle
                 Console.WriteLine("Poids: " + articleById.Poids);
                 Console.WriteLine("PrixInitial: " + articleById.PrixInitial);
                 Console.WriteLine("SKU: " + articleById.SKU);
+                Console.WriteLine("Date de sortie" + articleById.DateSortie);
                 Console.WriteLine("");
 
 
-                // Récupérer la totalité des articles (de trois façons : global + par étagère + par secteur)
+                // 2. Récupérer la totalité des articles
+                Console.WriteLine("***** Liste des articles *****");
+                var tableArticle = db.Article;
+                var listArticle =
+                    (from article in tableArticle
+                     select new {
+                         id = article.Id,
+                         libelle = article.Libelle,
+                         poids = article.Poids,
+                         prixinitial = article.PrixInitial,
+                         sku = article.SKU,
+                         datesortie = article.DateSortie,
+                     });
+
+                foreach (var article in listArticle)
+                {
+                    Console.WriteLine("*****" + article.id + "*****");
+                    Console.WriteLine(
+                    "Id : {0}, Libellé : {1}, SKU : {2}, Date de sortie : {3}, Prix initial : {4}, Poids : {5}",
+                    article.id, article.libelle, article.poids, article.prixinitial, article.sku, article.datesortie);
+                    Console.WriteLine("*******");
+                }
+                Console.WriteLine("");
 
 
-                // Ajouter/modifier/supprimer un article
+                // 3. Ajouter/modifier/supprimer un article
                 Console.WriteLine("Selection d'un article");
                 var articleRUD = db.Article
                     .OrderBy(b => b.Id)
                     .First();
 
+                Console.WriteLine("Modification de l'article");
                 articleRUD.Libelle = "Libelle modifié";
                 db.SaveChanges();
-                Console.WriteLine("Modification de l'article");
-                Console.WriteLine("");
 
+                Console.WriteLine("Suppression de l'article");
                 db.Remove(articleRUD);
                 db.SaveChanges();
-                Console.WriteLine("Suppression de l'article");
                 Console.WriteLine("");
 
-                // Récupérer le prix moyen par secteur
+                // 4. Récupérer le prix moyen par secteur
+                Console.WriteLine("***** Prix moyen par secteur *****");
+                var prixMoyenParSecteur =
+                    (from Article in tableArticle
+                     join Position in db.PositionMagasin on Article.Id equals Position.IdArticle
+                     join Etagere in db.Etagere on Position.IdEtagere equals Etagere.Id
+                     join Secteur in db.Secteur on Etagere.IdSecteur equals Secteur.Id
 
-                
+                     select new
+                     {
+                         nomSecteur = Secteur.Nom,
+                         moyenne = (Article.PrixInitial * Position.Quantite),
+                     });
+
+                foreach (var article in prixMoyenParSecteur)
+                {
+                    Console.WriteLine("Secteur : {0},  Moyenne : {1}",
+                        article.nomSecteur, article.moyenne);
+                }
             }
         }
     }
